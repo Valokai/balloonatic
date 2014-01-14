4b48e70ce4667f2c66037e96dbf56d5a5cdb0908
Valokai <cadmium_91@hotmail.com>
2014-01-14 16:23:29 +1300

    res is now 1000

--- src/state/MainGame.java
+++ src/state/MainGame.java
@@ -14,8 +14,8 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class MainGame extends StateBasedGame {
 
-    public static final int SCREEN_WIDTH = 8000;
-    public static final int SCREEN_HEIGHT = 720;
+    public static final int SCREEN_WIDTH = 1200;
+    public static final int SCREEN_HEIGHT = 920;
 
     public MainGame() {
         super("Balloonatic");
