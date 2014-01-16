341e87514fe65044296645ee9907294d23667736
Tin Htoo Aung <tinhtooaung@myanmarhub.net>
2014-01-16 14:47:20 +1300

    Implemented isCollided check in CollidableSprite

--- src/entities/CollidableSprite.java
+++ src/entities/CollidableSprite.java
@@ -1,5 +1,6 @@
 package entities;
 
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 
 import javax.imageio.ImageIO;
@@ -27,7 +28,6 @@ public abstract class CollidableSprite extends Sprite {
         super(x, y, imagePath);
     }
 
-
     // returns a HashSet of strings that list all the pixels in an image that aren't transparent
     // the pixels contained in the HashSet follow the guideline of:
     // x,y where x is the absolute x position of the pixel and y is the absolute y position of the pixel
@@ -52,9 +52,42 @@ public abstract class CollidableSprite extends Sprite {
                 }
             }
         }
-
         return mask;  //return our set
 
+    }
+
+    // Returns true if there is a collision between object a and object b
+    public boolean isCollided(CollidableSprite collidableSprite){
+
+        // This method detects to see if the images overlap at all. If they do, collision is possible
+        int ax1 = (int) getX();
+        int ay1 = (int) getY();
+        int ax2 = ax1 + getImage().getWidth();
+        int ay2 = ay1 + getImage().getHeight();
+        int bx1 = (int) collidableSprite.getX();
+        int by1 = (int) collidableSprite.getY();
+        int bx2 = bx1 + collidableSprite.getImage().getWidth();
+        int by2 = by1 + collidableSprite.getImage().getHeight();
+
+        if(by2 < ay1 || ay2 < by1 || bx2 < ax1 || ax2 < bx1){
+            return false; // Collision is impossible.
+        }
+        else{
+            // Collision is possible.
+            // get the masks for both images
+            HashSet<String> maskPlayer1 = getMask();
+            HashSet<String> maskPlayer2 = collidableSprite.getMask();
+
+            maskPlayer1.retainAll(maskPlayer2);  // Check to see if any pixels in maskPlayer2 are the same as those in maskPlayer1
+
+            if(maskPlayer1.size() > 0){  // if so, than there exists at least one pixel that is the same in both images, thus
+                if(onCollideListener != null){
+                    onCollideListener.onCollide(collidableSprite);
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     public interface OnCollideListener{
