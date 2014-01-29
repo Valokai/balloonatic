5b63aa0a557eb838ea73c592ce27924d62a64811
Valokai <cadmium_91@hotmail.com>
2014-01-29 14:49:41 +1300

    Merge branch 'dev' of https://github.com/tinhtooang/balloonatic into dev
    Conflicts:
    	src/game/MainState.java

--- src/game/MainState.java
+++ src/game/MainState.java
@@ -1,7 +1,4 @@
-package game; backlayer = new ScrollingHandler("background", new BackHills(0.0f,0,false,1)); //create back non collidable scrollable
-        backlayer.add(new BackHills(0.0f, 0, false, 2)); //add more map to the back scrollable
-        backlayer.add(new BackHills(0.0f, 0, false, 3)); //add more map to the back scrollable
-        backlayer.add(new BackHills(0.0f, 0, false, 4)); //add more map to the back scrollable
+package game;
 
 import graphic.Balloon;
 import graphic.Leaf;
@@ -15,6 +12,7 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import scrollables.BackHills;
 import scrollables.GreenHills;
+import scrollables.SecondHills;
 
 /**
  * Created with IntelliJ IDEA.
@@ -50,12 +48,15 @@ public class MainState extends BasicGameState {
         midlayer.add(new SecondHills(0.0f,0,false,3));
         midlayer.add(new SecondHills(0.0f,0,false,4));
 
-       balloon = (Balloon) sceneHandler.spawn(280, 100, Balloon.class, "balloon");
+        balloon = (Balloon) sceneHandler.spawn(280, 100, Balloon.class, "balloon");
+
         sceneHandler.spawn(280, 200, Leaf.class, "leaf");
-		 backlayer = new ScrollingHandler("background", new BackHills(0.0f,0,false,1)); //create back non collidable scrollable
+
+        backlayer = new ScrollingHandler("background", new BackHills(0.0f,0,false,1)); //create back non collidable scrollable
         backlayer.add(new BackHills(0.0f, 0, false, 2)); //add more map to the back scrollable
         backlayer.add(new BackHills(0.0f, 0, false, 3)); //add more map to the back scrollable
-        backlayer.add(new BackHills(0.0f, 0, false, 4)); //add more map to the back scrollable        skyimage = new Image("data/image/sky.png");
+        backlayer.add(new BackHills(0.0f, 0, false, 4)); //add more map to the back scrollable
+        skyimage = new Image("data/image/sky.png");
 
     }
 
@@ -63,7 +64,7 @@ public class MainState extends BasicGameState {
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         skyimage.draw(0, 0, MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT);
 
-sceneHandler.render(gameContainer, graphics);    //render the balloon
+
 /*render the scrollable layers*/
         backlayer.render();
         midlayer.render();
@@ -75,6 +76,7 @@ sceneHandler.render(gameContainer, graphics);    //render the balloon
         if(balloon.isCollided(sceneHandler.getSceneObjectById("leaf"))){
             graphics.drawString("Collided", 500, 300);
         }
+        sceneHandler.render(gameContainer, graphics);    //render the balloon
     }
 
     @Override
