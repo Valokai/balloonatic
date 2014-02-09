1393431adc88f52576ef3c7310f39aa0c8a46339
Valokai <cadmium_91@hotmail.com>
2014-02-10 11:53:20 +1300

    fullscreen removed

--- src/game/MainGame.java
+++ src/game/MainGame.java
@@ -61,7 +61,7 @@ public class MainGame extends StateBasedGame {
 
         }
         AppGameContainer app = new AppGameContainer(new MainGame());
-        app.setDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, true);
+        app.setDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, false);
         app.setVSync(Game.VSYNC);
         //app.setShowFPS(Game.DEBUG.SHOW_FPS);
         app.start();
