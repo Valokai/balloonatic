5ce1fde460dd99ce57cfe5fcc158128bf4eacced
Valokai <cadmium_91@hotmail.com>
2014-02-11 14:40:26 +1300

    dhac

---
+++ src/scrollables/CaveSecond.java
@@ -0,0 +1,35 @@
+package scrollables;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import graphic.*;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user360
+ * Date: 28/01/14
+ * Time: 12:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CaveSecond extends Background {
+
+    public CaveSecond(float x, float y, boolean collidable, int type) throws SlickException {
+        super(x, y, "data/image/backgrounds/cave_bg1.png", collidable);
+
+        if(type==1) {
+            this.setImage("data/image/backgrounds/cave_bg1.png");
+        }
+        if(type==2) {
+            this.setImage("data/image/backgrounds/cave_bg1.png");
+        }
+        if(type==3) {
+            this.setImage("data/image/backgrounds/cave_bg1.png");
+        }
+        if(type==4) {
+            this.setImage("data/image/backgrounds/cave_bg1.png");
+        }
+
+    }
+
+}
